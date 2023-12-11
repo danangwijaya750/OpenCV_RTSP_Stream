02f68e4ec1ceef93caa5c7d7064c578baf9f0096
Danang Wijaya <danangwijaya750@gmail.com>
2023-12-11 22:45:39 +0800

    fix: fix code

--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 # OpenCV RTSP Streaming Sever
+## Libraries Installation
+    `
+    $ sudo apt update
+    $ sudo apt-get install libgstrtspserver-1.0-0 gstreamer1.0-rtsp
+    For gst-rtsp-server (and other GStreamer stuff) to be accessible in
+    Python through gi.require_version(), it needs to be built with
+    gobject-introspection enabled (libgstrtspserver-1.0-0 is already).
+    Yet, we need to install the introspection typelib package:
+    $ sudo apt-get install libgirepository1.0-dev
+    $ sudo apt-get install gobject-introspection gir1.2-gst-rtsp-server-1.0
+    `
+## Usa
--- stream_server.py
+++ stream_server.py
@@ -6,36 +6,36 @@ gi.require_version('GstRtspServer', '1.0')
 from gi.repository import GObject, Gst, GstRtspServer
 
 def main():
+    ip='192.168.88.227'
+    rtsp_port_num = 8554 
+    video_source="video/RGBT_RGB.mp4"
     
     out_send = cv2.VideoWriter('appsrc is-live=true ! videoconvert ! \
                                 omxh264enc bitrate=12000000 ! video/x-h264, \
                                 stream-format=byte-stream ! rtph264pay pt=96 ! \
-                                udpsink host=192.168.88.227 port=8554 async=false',
+                                udpsink host='+ip+' port='+str(rtsp_port_num)+ ' async=false',
                                 cv2.CAP_GSTREAMER, 0, 30, (1920,1080), True)
 
     if not out_send.isOpened():
         print('VideoWriter not opened')
         exit(0)
  
-    rtsp_port_num = 8554 
- 
     server = GstRtspServer.RTSPServer.new()
     server.props.service = "%d" % rtsp_port_num
     server.attach(None)
     
     factory = GstRtspServer.RTSPMediaFactory.new()
-    factory.set_launch("(udpsrc name=pay0 port=8554 buffer-size=524288 \
+    factory.set_launch("(udpsrc name=pay0 port="+str(rtsp_port_num)+" buffer-size=524288 \
                         caps=\"application/x-rtp, media=video, clock-rate=90000, \
                         encoding-name=(string)H264, payload=96 \")")
 
     factory.set_shared(True)
     server.get_mount_points().add_factory("/test", factory)
  
-    print("\n *** Launched RTSP Streaming at rtsp://192.168.88.227:%d/test ***\n\n" % rtsp_port_num)    
+    print("\n *** Launched RTSP Streaming at rtsp://"+ip+":%d/test ***\n\n" % rtsp_port_num)    
     
     while True :
-        cap = cv2.VideoCapture("video/RGBT_RGB.mp4")
-        #cap = cv2.VideoCapture("video/goat_cat_meme.mp4")
+        cap = cv2.VideoCapture(video_source)
     
         while True:
             ret, mat = cap.read()
